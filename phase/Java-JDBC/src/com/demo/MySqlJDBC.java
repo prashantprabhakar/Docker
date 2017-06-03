5a29cb9fd8159588e2a243a414bb1de6f6440f46
Prashant Prabhakar Singh <prashantprabhakar123@gmail.com>
2017-06-04 01:54:57 +0530

    Docker demo:

---
+++ phase/Java-JDBC/src/com/demo/MySqlJDBC.java
@@ -0,0 +1,29 @@
+package com.demo;
+
+import java.sql.Connection;
+//import com.mysql.jdbc.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MySqlJDBC {
+
+	public static void main(String[] args) {
+		/*try{
+			//Class.forName("com.mysql.jdbc.Driver");
+			// here testEmp is DB name and prsingh is user and Gemini@123 is password
+			Connection con = DriverManager.getConnection("jdbc:mysql://10.10.20.173:3306/testDBByPrashant","prsingh","Gemini@123");
+			Statement stmt = con.createStatement();
+			ResultSet rs=stmt.executeQuery("select * from testTableByPrashant");
+			while(rs.next()){
+				System.out.println(rs.getInt(1)+" "+rs.getString(2));			
+			}
+			con.close();			
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}*/
+
+	}
+
+}
